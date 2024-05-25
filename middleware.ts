d5b7c69a7e9480384d39d432a7e841469f54795a
Labeeb Nixxie <labeeb@nixxieinternational.com>
2024-05-25 16:26:05 +0400

    first commit

---
+++ middleware.ts
@@ -0,0 +1,21 @@
+import { clerkMiddleware } from "@clerk/nextjs/server";
+ 
+export default clerkMiddleware({
+  publicRoutes: [
+    '/',
+    '/events/:id',
+    '/api/webhook/clerk',
+    '/api/webhook/stripe',
+    '/api/uploadthing'
+  ],
+  ignoredRoutes: [
+    '/api/webhook/clerk',
+    '/api/webhook/stripe',
+    '/api/uploadthing'
+  ]
+});
+ 
+export const config = {
+  matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
+};
+ 
